90a7d89e9737552362905bf900361cd2fa52f16a
David Mora Perea <100316381+David-MoraP@users.noreply.github.com>
2022-03-05 16:14:24 -0800

    Delete wallpaper_test.py

--- wallpaper_test.py
+++
@@ -1,8 +0,0 @@
-import ctypes
-
-class Wallpaper:
-    def __init__(self, pp):
-        ctypes.windll.user32.SystemParametersInfoW(20, 0, pp, 0)
-
-path = 'e:/Theseus/Pictures/NASA_APOD/2022_02_27_NASA_APOD.png'
-application = Wallpaper(path)
